2567c2fb5bedc0a183b61f94d7f7f1f101ff6c0b
finnb <finnb@192.168.0.142>
2026-03-18 11:55:23 +0000

    Vehicle Class

--- src/projectdsaa/Vehicle.java
+++ src/projectdsaa/Vehicle.java
@@ -9,5 +9,37 @@ package projectdsaa;
  * @author finnb
  */
 public class Vehicle {
+    protected String VehicleType;
+    protected int vehicleId;
+   
+    public Vehicle(){}
+    public Vehicle(int vehicleId,String VehicleType){
+        
+    }
+
+    public String getVehicleType() {
+        return VehicleType;
+    }
+
+    public void setVehicleType(String VehicleType) {
+        this.VehicleType = VehicleType;
+    }
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" + "VehicleType=" + VehicleType + ", vehicleId=" + vehicleId + '}';
+    }
+    
+    
+    
+    
     
 }
